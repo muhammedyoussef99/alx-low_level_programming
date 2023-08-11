36bccbc23524b92d3a14f2eef9ea4f732e82a401
muhammed <muhammedyoussef321@gmail.com>
2023-08-11 08:42:43 -0700

    nj

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function is the entry point of the program.
+ *              It prints a specific message to the console.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
