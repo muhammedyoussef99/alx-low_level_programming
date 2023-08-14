3de55002c172af94497ea9231f1d63cafc9aab8c
muhammed <muhammedyoussef321@gmail.com>
2023-08-14 07:39:02 -0700

    nj

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,18 +12,17 @@
 int main(void)
 {
 	int num1 = 0;
-	int num2;
+	int num2 = 1;
 
 	while (num1 <= 98)
 	{
-		num2 = num1 + 1;
-		while (num2 <= 9)
+		while (num2 <= 99)
 		{
-			putchar(num1 / 10 + '0');
-			putchar(num1 % 10 + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar(num2 / 10 + '0');
-			putchar(num2 % 10 + '0');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 != 98 || num2 != 99)
 			{
@@ -34,6 +33,7 @@ int main(void)
 			num2++;
 		}
 		num1++;
+		num2 = num1 + 1;
 	}
 
 	putchar('\n');
