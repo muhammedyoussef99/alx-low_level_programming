50f2673ae067787aecd1bb47d7ca09d65c64ed2a
muhammed <muhammedyoussef321@gmail.com>
2023-08-14 06:37:02 -0700

    nj

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of two digits
+ *              in ascending order.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int first_digit = 0;
+	int second_digit;
+
+	while (first_digit <= 8)
+	{
+		second_digit = first_digit + 1;
+		while (second_digit <= 9)
+		{
+			putchar(first_digit + '0');
+			putchar(second_digit + '0');
+
+			if (first_digit != 8 || second_digit != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			second_digit++;
+		}
+		first_digit++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
