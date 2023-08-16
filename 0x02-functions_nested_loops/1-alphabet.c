4bb621ab03db41ad6d0cdef0631e5fb2902e276c
muhammed <muhammedyoussef321@gmail.com>
2023-08-15 18:28:50 -0700

    nj

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+}
+	_putchar('\n');
+}
