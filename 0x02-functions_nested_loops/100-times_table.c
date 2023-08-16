b16e9da81e05fb232811c599c45ed477c79b28ff
muhammed <muhammedyoussef321@gmail.com>
2023-08-16 13:22:18 -0700

    nj

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,33 +3,33 @@
 /**
  * print_times_table - Prints the n times table
  * @n: The number for which the times table is printed
- */
+*/
 
 void print_times_table(int n)
 {
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= n; row++)
-    {
-        _putchar('0');
-        for (column = 1; column <= n; column++)
-        {
-            _putchar(',');
-            _putchar(' ');
+	for (row = 0; row <= n; row++)
+	{
+		_putchar('0');
+		for (column = 1; column <= n; column++)
+		{
+			_putchar(',');
+			_putchar(' ');
 
-            product = row * column;
+			product = row * column;
 
-            if (product <= 9)
-                _putchar(' ');
-            else
-                _putchar((product / 10) + '0');
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
 
-            _putchar((product % 10) + '0');
-        }
+			_putchar((product % 10) + '0');
+		}
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
