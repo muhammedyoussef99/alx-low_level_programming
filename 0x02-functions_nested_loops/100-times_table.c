cc2082444e19e9e8de934de386d56bbae18e9a2b
muhammed <muhammedyoussef321@gmail.com>
2023-08-16 13:47:51 -0700

    nj

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,19 +29,19 @@ void print_times_table(int n)
 			{
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(product + '0');
+				_putchar(product + 0);
 			}
 			else if (product <= 99)
 			{
 				_putchar(' ');
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+				_putchar((product / 10) + 0);
+				_putchar((product % 10) + 0);
 			}
 			else
 			{
-				_putchar((product / 100) + '0');
-				_putchar(((product / 10) % 10) + '0');
-				_putchar((product % 10) + '0');
+				_putchar((product / 100) + 0);
+				_putchar(((product / 10) % 10) + 0);
+				_putchar((product % 10) + 0);
 			}
 		}
 		_putchar('\n');
