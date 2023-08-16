ddfb1b767add585c24b3e8ffee01620b1ca8ad32
muhammed <muhammedyoussef321@gmail.com>
2023-08-16 13:39:10 -0700

    nj

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,24 +12,38 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 		return;
 
-	for (row = 0; row <= n; ++row)
-	{
-		_putchar('0');
-		for (column = 1; column <= n; ++column)
-		{
-			_putchar(',');
-			_putchar(' ');
 
+	for (row = 0; row <= n; row++)
+	{
+		for (column = 0; column <= n; column++)
+		{
 			product = row * column;
 
-			if (product <= 9)
+			if (column > 0)
+			{
+				_putchar(',');
 				_putchar(' ');
-			else
+			}
+
+			if (product <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (product <= 99)
+			{
+				_putchar(' ');
 				_putchar((product / 10) + '0');
-
-			_putchar((product % 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar((product / 100) + '0');
+				_putchar(((product / 10) % 10) + '0');
+				_putchar((product % 10) + '0');
+			}
 		}
-
 		_putchar('\n');
 	}
 }
