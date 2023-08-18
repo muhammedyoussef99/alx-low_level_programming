73bf9afa3ae2cd448371688c7e262fb668a26058
muhammed <muhammedyoussef321@gmail.com>
2023-08-17 18:01:56 -0700

    nj

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,14 +15,14 @@ void print_diagonal(int n)
 		_putchar('\n');
 	}
 	else
-	{        
+	{
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
-			_putchar('\\'); /*Print the '\' character for the diagonal*/
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
