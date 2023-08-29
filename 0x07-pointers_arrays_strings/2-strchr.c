c1e6f624524f1f1a560e5fec27e444850c046bc7
muhammed <muhammedyoussef321@gmail.com>
2023-08-28 17:52:17 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,9 +12,9 @@ char *_strchr(char *s, char c)
 {
 	int ptr;
 
-	for (ptr = 0; s[ptr] >= '\0'; ptr++)
+	for (ptr = 0; s[ptr] >= '\0' ; ptr++)
 	{
-		if (*s == c)
+		if (s[ptr] == c)
 		{
 			return (s + ptr);
 		}
