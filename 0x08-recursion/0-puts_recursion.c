8cfbf700cab2970b40c02344567a4396191f943d
muhammed <muhammedyoussef321@gmail.com>
2023-09-01 00:57:02 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,9 +10,11 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		putchar('\n');
-		return;
-	}
 
-	putchar(*s);
-	_puts_recursion(s + 1);
+	}
+	else;
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
