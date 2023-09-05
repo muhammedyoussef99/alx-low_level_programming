13aadc4e483239b7601b0a48b2412add7ea91217
muhammed <muhammedyoussef321@gmail.com>
2023-09-05 08:35:17 -0700

    malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - finds the length of the string
+ * @s: string
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+/**
  * argstostr - concatenates all the arguments of your program
  * @ac: the number of arguments
  * @av: an array of strings containing the arguments
@@ -12,37 +26,27 @@ char *argstostr(int ac, char **av)
 {
 	int total_length = 0;
 	int x, y, z = 0;
-	char *cd = malloc((total_length + 1) * sizeof(char));
+	char *cd;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (x = 0; x < ac; x++)
-	{
-		for (y = 0; av[x][y]; y++)
-			total_length++;
-	}
+	for (; x < ac; x++; total_length++)
+		total_length += _strlen(av[x]);
 
-	total_length += ac;
+	cd = malloc((total_length + 1) * sizeof(char));
 
-	if (cd == NULL)
+	if (cd == 0)
 		return (NULL);
 
 	for (x = 0; x < ac; x++)
 	{
-		for (y = 0; av[x][y]; y++)
-		{
+		for (y = 0; av[x][y] != '\0'; y++; z++)
 			cd[z] = av[x][y];
-			z++;
-		}
-		if (x < ac - 1)
-		{
-			cd[z] = '\n';
-			z++;
-		}
+
+		cd[z] = '\n';
+		z++;
 	}
-
 	cd[z] = '\0';
-
 	return (cd);
 }
