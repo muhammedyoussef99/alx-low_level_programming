e0f31184f32885ba26f62754cb47855592f8e80f
muhammed <muhammedyoussef321@gmail.com>
2023-09-23 08:50:56 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,74 +1,54 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * reverse_string - Reverses a string in-place.
- * @str: The string to be reversed.
-*/
-void reverse_string(char *str)
-{
-	int length = strlen(str);
-	int start = 0;
-	int end = length - 1;
-
-	while (start < end)
-	{
-		char temp = str[start];
-
-		str[start] = str[end];
-		str[end] = temp;
-		start++;
-		end--;
-	}
-}
-
-/**
- * infinite_add - Adds two positive numbers represented as strings.
- * @n1: The first number as a string.
- * @n2: The second number as a string.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
  *
- * Return: A pointer to the result if successful, otherwise 0.
 */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0;
-	int idx_r = 0;
+	/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-	int len_n1 = strlen(n1);
-	int len_n2 = strlen(n2);
-
-	while (idx_r < size_r - 1)
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
+		return (0);
+	r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
-		int num1 = (idx_r < len_n1) ? n1[len_n1 - idx_r - 1] - '0' : 0;
-		int num2 = (idx_r < len_n2) ? n2[len_n2 - idx_r - 1] - '0' : 0;
-		int sum = num1 + num2 + carry;
-
-		carry = sum / 10;
-		r[idx_r] = (sum % 10) + '0';
-
-		idx_r++;
-
-		if ((idx_r >= len_n1 || idx_r >= len_n2) && carry == 0)
-			break;
-	}
-
-	if (carry)
-	{
-		if (idx_r < size_r - 1)
-		{
-			r[idx_r] = carry + '0';
-			idx_r++;
-		}
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
 		else
-		{
-			return (0);
-		}
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
 	}
-
-	r[idx_r] = '\0';
-	reverse_string(r);
+	if (d == 1)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
+	}
 	return (r);
 }
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,49 +1,82 @@
-#iinclude "main.h"
+#include "main.h"
 #include <stdio.h>
-#include <ctype.h>
 
 /**
- * print_buffer - Prints the content of a buffer.
- * @b: The buffer to be printed.
- * @size: The size of the buffer.
+ * isPrintableASCII - determines if n is a printable ASCII char
+ * @n: integer
+ * Return: 1 if true, 0 if false
 */
+
+int isPrintableASCII(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * printHexes - print hex values for string b in formatted form
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+*/
+
+void printHexes(char *b, int start, int end)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+
+/**
+ * printASCII - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+*/
+
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: string
+ * @size: size of buffer
+*/
+
 void print_buffer(char *b, int size)
 {
-	if (size <= 0)
+	int start, end;
+
+	if (size > 0)
 	{
-		putchar('\n');
-		return;
-	}
-	for (int i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
-
-		for (int j = 0; j < 10; j++)
+		for (start = 0; start < size; start += 10)
 		{
-			if (i + j < size)
-				printf("%02x", (unsigned char)b[i + j]);
-			else
-				printf("  ");
-
-			if (j % 2 == 1)
-				printf(" ");
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
 		}
-
-		for (int j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-			{
-				if (isprint(b[i + j]))
-					putchar(b[i + j]);
-				else
-					putchar('.');
-			}
-			else
-			{
-				putchar(' ');
-			}
-		}
-
-		putchar('\n');
-	}
+	} else
+		printf("\n");
 }
